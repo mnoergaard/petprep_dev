a9f1b38d2850cd1f7134908413da4d92c1640db5
mnoergaard <martin.noergaard@nru.dk>
2025-07-25 23:16:15 +0200

    FIX: remove blood derivatives argument

--- petprep/cli/parser.py
+++ petprep/cli/parser.py
@@ -617,14 +617,6 @@ https://petprep.readthedocs.io/en/%s/spaces.html"""
         choices=['logan', 'ma1', '1tcm', '2tcm'],
         help='Kinetic model(s) to fit',
     )
-    g_kin.add_argument(
-        '--blood-derivatives',
-        default='bloodstream',
-        help=(
-            'Path or derivative name containing blood data '
-            '(e.g., --blood-derivatives /path/to/bloodstream)'
-        ),
-    )
     g_kin.add_argument('--tstar', type=float, help='t* for linear models')
     g_kin.add_argument('--vb-fixed', type=float, help='Fixed blood volume fraction')
     g_kin.add_argument('--fit-end-time', type=float, help='End time for fitting (minutes)')
